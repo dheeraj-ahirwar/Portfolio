68736dc1ccbea2cf70cf5142e21529bf026a22f9
Dheeraj Ahirwar <133276649+dheeraj-ahirwar@users.noreply.github.com>
2024-06-10 20:52:50 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// var typed =new Typed(".text",{
+//     Strings:["Frontend Developer", "Youtuber","Web Developer"],
+//     typeSpeed:100,
+//     backSpeed: 100,
+//     backDelay:1000,
+//     loop:true
+// }); 
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
